1d90ca51916b975f95d1b8f27054e6f6d1aaffd5
Karan Mehta <karanmehta0714@gmail.com>
2025-09-07 23:57:24 +0530

    Add files via upload

---
+++ typeconversion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;
+    float b = 6.8;
+    printf("the value of a divide by b is %f \n",a/b);
+    int k = 8.9;
+    printf("the value of k is %d",k);//if any float number is intialize as int then it will be like this 8
+    return 0;
+}
